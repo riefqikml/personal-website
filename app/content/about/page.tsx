3708225e347cbb9047bdf215d9274ccef866af4f
Riefqi Kamal <riefqikamal369@gmail.com>
2024-02-17 10:58:36 +0800

    adjust text align for paragraph

--- app/content/about/page.tsx
+++ app/content/about/page.tsx
@@ -5,8 +5,8 @@ const About = () => {
     <>
       <div className="p-4">
         <h1 className="text-3xl font-bold text-center">About Me</h1>
-        <div className="p-6 md:px-72 pt-6">
-          <p className="text-justify">
+        <div className="p-6 lg:px-20 pt-6">
+          <p className="text-justify sm:text-center">
             Hello, I am a self-taught programmer since my teenage years. I enjoy
             building various things, from static websites to A.I. tools. I have
             always loved what I do.
