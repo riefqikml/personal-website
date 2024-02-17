2b8e547bd97c60282a0f215200c691d2a52659ea
Riefqi Kamal <riefqikamal369@gmail.com>
2024-02-17 08:55:48 +0800

    fix: dropdown menu

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -2,36 +2,32 @@ import Link from "next/dist/client/link";
 
 export const Navbar = () => {
   return (
-    <>
-      <div className="navbar bg-base-100 p-4 lg:px-8">
-        <div className="flex-1">
-          <Link href="/" className="btn btn-ghost text-xl">
-            Riefqi
-          </Link>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
+    <div className="navbar bg-base-100 p-4 lg:px-8">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost text-xl rounded-box">
+          Riefqi
+        </Link>
+      </div>
+      <div className="flex-row">
+        <Link href="/content/about" className="btn btn-ghost rounded-box">
+          <div tabIndex={0} role="button">
+            About
+          </div>
+        </Link>
+        <div className="dropdown dropdown-hover">
+          <p tabIndex={0} role="button" className="btn m-2 rounded-box">Others</p>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow-xl bg-base-200 rounded-box">
             <li>
-              <Link href="/content/about">About</Link>
+              <Link href="https://github.com/feir-afk" target="_blank">
+                Github
+              </Link>
             </li>
-            <li className="">
-              <details>
-                <summary>Others</summary>
-                <ul className="p-2 bg-base-200 rounded-xl shadow-xl z-10">
-                  <li>
-                    <Link href="https://github.com/feir-afk" target="_blank">
-                      Github
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/content/projects">Projects</Link>
-                  </li>
-                </ul>
-              </details>
+            <li>
+              <Link href="/content/projects">Projects</Link>
             </li>
           </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
