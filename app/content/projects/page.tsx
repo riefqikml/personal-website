a2ac27004a369369e00764ecbda88cf2014b4dbb
Riefqi Kamal <riefqikamal369@gmail.com>
2024-02-23 18:36:08 +0800

    fix deployment: remove [id] folder.

--- app/content/projects/page.tsx
+++ app/content/projects/page.tsx
@@ -1,10 +1,42 @@
-import ProjectPage from "./[id]/page";
+import { projectList } from "@/projects-data";
+import Link from "next/link";
+import { GoLink } from "react-icons/go";
+import { FaGithub } from "react-icons/fa";
 
 const Projects = () => {
 	return (
 		<div>
 			<h1 className="text-center pb-8 pt-4 text-3xl font-bold">Projects</h1>
-			<ProjectPage />
+			<div className="flex flex-wrap px-4 pt-4 pb-8 gap-14 justify-center items-center">
+				{projectList.map((item: any) => (
+					<>
+						<div className="rounded-badge w-96 bg-base-200 shadow-xl scale-100 hover:scale-105 ease-in duration-200">
+							<div className="card-body">
+								<h2 className="card-title">{item.name}</h2>
+								<p className="h-[60px]">{item.description}</p>
+								<div className="card-actions justify-end">
+									<Link href={item.link} target="_blank">
+										<button
+											className="btn btn-ghost rounded-box tooltip"
+											data-tip="Project Website"
+										>
+											<GoLink />
+										</button>
+									</Link>
+									<Link href={item.github} target="_blank">
+										<button
+											className="btn btn-ghost rounded-box tooltip"
+											data-tip="Source Code"
+										>
+											<FaGithub />
+										</button>
+									</Link>
+								</div>
+							</div>
+						</div>
+					</>
+				))}
+			</div>
 		</div>
 	);
 };
