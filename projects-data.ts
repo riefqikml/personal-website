49c737060f77633409f3b395b69c57154f0b70f0
Riefqi <riefqikamal369@gmail.com>
2024-05-26 04:20:41 +0800

    Update Github link

--- projects-data.ts
+++ projects-data.ts
@@ -1,14 +1,14 @@
 export const projectList = [
-	{
-		name: "Simple Task App",
-		description: "A simple way to create, read, update and delete tasks",
-		link: "https://simple-task-demo.vercel.app/",
-		github: "https://github.com/feir-afk/task-app",
-	},
-	{
-		name: "Next Authentication",
-		description: "Build authentication using NextAuth V5",
-		link: "https://authjs-demo.vercel.app/",
-		github: "https://github.com/feir-afk/authentication",
-	},
+  {
+    name: "Simple Task App",
+    description: "A simple way to create, read, update and delete tasks",
+    link: "https://simple-task-demo.vercel.app/",
+    github: "https://github.com/riefqikml/task-app",
+  },
+  {
+    name: "Next Authentication",
+    description: "Build authentication using NextAuth V5",
+    link: "https://authjs-demo.vercel.app/",
+    github: "https://github.com/riefqikml/authentication",
+  },
 ];
