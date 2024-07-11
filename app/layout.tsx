b2d0d862cd22775616b8e9549147c0b569a9378e
Riefqi <riefqikamal369@gmail.com>
2024-07-11 16:29:04 +0800

    Change theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="acid">
+    <html lang="en" data-theme="halloween">
       <body className={firaCode.className}>
         <Navbar />
         {children}
