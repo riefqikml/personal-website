2900bb308b0a43cc6b7bcbf0362c68fe9d2d293e
Riefqi <riefqikamal369@gmail.com>
2024-07-11 16:05:31 +0800

    Fix: Each child in a list should have a unique “key” prop.

--- app/content/projects/page.tsx
+++ app/content/projects/page.tsx
@@ -9,8 +9,7 @@ const Projects = () => {
 			<h1 className="text-center pb-8 pt-4 text-3xl font-bold">Projects</h1>
 			<div className="flex flex-wrap gap-14 justify-center items-center p-8 pt-4">
 				{projectList.map((item: any) => (
-					<>
-						<div className="rounded-badge w-96 bg-base-200 shadow-xl scale-100 hover:scale-105 ease-in duration-200">
+						<div key={item.id} className="rounded-badge w-96 bg-base-200 shadow-xl scale-100 hover:scale-105 ease-in duration-200">
 							<div className="card-body">
 								<h2 className="card-title">{item.name}</h2>
 								<p className="h-[60px]">{item.description}</p>
@@ -34,7 +33,6 @@ const Projects = () => {
 								</div>
 							</div>
 						</div>
-					</>
 				))}
 			</div>
 		</div>
--- projects-data.ts
+++ projects-data.ts
@@ -1,11 +1,13 @@
 export const projectList = [
   {
+    id: 1,
     name: "Simple Task App",
     description: "A simple way to create, read, update and delete tasks",
     link: "https://simple-task-demo.vercel.app/",
     github: "https://github.com/riefqikml/task-app",
   },
   {
+    id: 2,
     name: "Next Authentication",
     description: "Build authentication using NextAuth V5",
     link: "https://authjs-demo.vercel.app/",
